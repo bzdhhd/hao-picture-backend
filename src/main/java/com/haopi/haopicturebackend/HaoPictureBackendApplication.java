c3f9384ce6c894753705747cb9c633c043e0d04f
mxk-zwh <2690228595@qq.com>
2025-01-26 23:05:57 +0800

    初始化

---
+++ src/main/java/com/haopi/haopicturebackend/HaoPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.haopi.haopicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haopi/haopicturebackend/HaoPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haopi.haopicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
